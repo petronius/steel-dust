ea4a34b27b24a987c36ffc6dba1eca78a0649060
Michael Schuller <michael.schuller@bcgdv.com>
2021-08-06 17:07:32 +0200

    Level refactoring

--- engine/game.py
+++ engine/game.py
@@ -1,22 +1,26 @@
 import pyglet
 
-import engine.menu
+import engine.screens.menu
+import engine.screens.level as level
 import engine.camera
-import engine.level as level
+
 
 class Game(object):
     def __init__(self):
-        self.current_screen = engine.menu.MainMenu(self)
+        self.current_screen = engine.screens.menu.MainMenu(self)
+
 
     def clearCurrentScreen(self):
         self.current_screen.clear()
         self.window.remove_handler("on_key_press", self.current_screen.on_key_press)
         self.window.remove_handler("on_draw", self.current_screen.on_draw)
 
+
     def startCurrentScreen(self):
         self.window.set_handler("on_key_press", self.current_screen.on_key_press)
         self.window.set_handler("on_draw", self.current_screen.on_draw)
         self.current_screen.start()
+
 
     def startPlaying(self):
         self.clearCurrentScreen()
