0a7623988bbfe7e84cba86584ed55410ace483f1
Michael Schuller <michael.schuller@bcgdv.com>
2021-08-17 17:05:12 +0200

    Generator bug annoying bullshit

--- engine/server.py
+++ engine/server.py
@@ -56,7 +56,6 @@ class GameServer(Server):
     def SendPlayers(self):
         # don't send players that aren't fully initialized yet
         players = filter(lambda p: p.uuid not in ("", "(unk)"), self.players)
-        print("Broadcasting player list: %s" % [p.uuid for p in players])
         self.SendToAll({
             "action": "players",
             "players": [{
@@ -67,7 +66,6 @@ class GameServer(Server):
         })
 
     def SendToAll(self, data):
-        print("wall", data)
         for p in self.players:
             p.Send(data)
 
